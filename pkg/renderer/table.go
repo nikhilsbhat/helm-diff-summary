223466587fb205019c038bbefbe505fc692faf74
nikhilsbhat <nikhilsbhat93@gmail.com>
2026-05-11 11:14:19 +0530

    feat: add terraform-style helm diff summarization with CLI help and plan output

--- pkg/renderer/table.go
+++ pkg/renderer/table.go
@@ -1,6 +1,7 @@
 package renderer
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/jedib0t/go-pretty/v6/table"
@@ -19,6 +20,12 @@ func Render(resources []parser.ResourceDiff) {
 		"CHANGES",
 	})
 
+	var (
+		creates int
+		updates int
+		deletes int
+	)
+
 	for _, resource := range resources {
 		tableWriter.AppendRow(table.Row{
 			resource.Kind,
@@ -27,7 +34,20 @@ func Render(resources []parser.ResourceDiff) {
 			resource.ChangeType,
 			resource.ChangedLines,
 		})
+
+		switch resource.ChangeType {
+		case parser.Create:
+			creates++
+		case parser.Update:
+			updates++
+		case parser.Delete:
+			deletes++
+		}
 	}
 
 	tableWriter.Render()
+
+	fmt.Println()
+
+	fmt.Printf("Plan: %d to create, %d to update, %d to delete.\n", creates, updates, deletes)
 }
